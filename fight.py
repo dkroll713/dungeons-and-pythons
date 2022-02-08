59cd93a2b28593188ca7e402c996572d7c719ee0
dkroll713 <dkroll713@gmail.com>
2022-02-08 17:07:06 -0600

    streamlined output

--- fight.py
+++ fight.py
@@ -1,5 +1,6 @@
 import random
 import time
+import pickle
 
 def multiply(x,y):
     return x*y
@@ -8,7 +9,42 @@ fighters = []
 hit_point_values = [3,4,5]
 ads_values = [1,2,3,4,5,6,7,8,9,10]
 avoid_values = [.1,.2,.3,.4,.5,.6]
+block_values = [.6, .7, .8, .9, 1, 1.1, 1.2, 1.3, 1.4, 1.5]
 names = ['Dad', 'Mom', 'Joe', 'John', 'Toohey', 'Earle', 'Drake', 'Tripp', 'X']
+
+def printSelf():
+    print("\n")
+    print("Your fighter: "+str(a.name))
+    time.sleep(.5)
+    print("Attack stat: "+str(a.attack))
+    time.sleep(.5)
+    print("Defense stat: "+str(a.defense))
+    time.sleep(.5)
+    print("Speed stat: "+str(a.speed))
+    time.sleep(.5)
+    print("Hit points: "+str(a.hit_points))
+    time.sleep(.5)
+    if a.pet is True:
+        print("Has a pet")
+    else:
+        pass
+
+def printOpponent():
+    print("\n")
+    print("Your opponent: "+str(b.name))
+    time.sleep(.5)
+    print("Attack stat: "+str(b.attack))
+    time.sleep(.5)
+    print("Defense stat: "+str(b.defense))
+    time.sleep(.5)
+    print("Speed stat: "+str(b.speed))
+    time.sleep(.5)
+    print("Hit points: "+str(b.hit_points))
+    time.sleep(.5)
+    if b.pet is True:
+        print("Has a pet")
+    else:
+        pass
 
 class Fighter:
     type = 'Fighter'
@@ -20,6 +56,14 @@ class Fighter:
         self.speed = speed
         self.pet = pet
         self.status = status
+
+    def save(self):
+        with open("save.pk1", "wb") as fp:
+            pickle.dump(self, fp, protocol=pickle.HIGHEST_PROTOCOL)
+
+def load():
+    with open("save.pk1", "rb") as fp:
+        return pickle.load(fp)
 
 class Opponent:
     type = 'Opponent'
@@ -36,21 +80,22 @@ a = Fighter
 b = Opponent
 
 def create():
-    print("Choose your character:")
-    a.name = input("Name: ")
-    a.attack = int(input("Choose your attack stat, 1-10: "))
-    a.defense = int(input("Choose your defense stat, 1-10: "))
+    print("Welcome to DUNGEONS AND PYTHONS")
+    print("\nChoose your character:")
+    a.name = input("\nName: ")
+    a.attack = int(input("\nChoose your attack stat, 1-10: "))
+    a.defense = int(input("\nChoose your defense stat, 1-10: "))
     a.hit_points =multiply(multiply(a.defense,random.choice(hit_point_values)),5)
     a.speed = random.choice(ads_values)
     a.pet = False
     a.status = True
 
-    print(a.name)
-    print(a.attack)
-    print(a.defense)
-    print(a.hit_points)
-    if a.status is True:
-        print("It's ALIIIIIIIIIIVE")
+    # print(a.name)
+    # print(a.attack)
+    # print(a.defense)
+    # print(a.hit_points)
+    # if a.status is True:
+    #     print("It's ALIIIIIIIIIIVE")
 
 def create_opponents():
     b.name = random.choice(names)
@@ -61,8 +106,8 @@ def create_opponents():
     b.pet = False
     b.status = True
     if b.status is True:
-        print("Prepare to battle")
-    print(b.name,b.attack,b.defense,b.hit_points)
+        print("\n\nPrepare to battle")
+    # print(b.name,b.attack,b.defense,b.hit_points)
 
 def create_tournament_fighters():
     x = int(input("How many fighters are you willing to face? "))+1
@@ -73,30 +118,6 @@ def create_tournament_fighters():
     for i in range(1,x):
         print(vars(fighters[y]))
         y = y+1
-
-def printSelf():
-    print("\n")
-    print("Your fighter: "+str(a.name))
-    print("Attack stat: "+str(a.attack))
-    print("Defense stat: "+str(a.defense))
-    print("Speed stat: "+str(a.speed))
-    print("Hit points: "+str(a.hit_points))
-    if a.pet is True:
-        print("Has a pet")
-    else:
-        pass
-
-def printOpponent():
-    print("\n")
-    print("Your opponent: "+str(b.name))
-    print("Attack stat: "+str(b.attack))
-    print("Defense stat: "+str(b.defense))
-    print("Speed stat: "+str(b.speed))
-    print("Hit points: "+str(b.hit_points))
-    if b.pet is True:
-        print("Has a pet")
-    else:
-        pass
 
 #_dmg = damage points : attack stat * d6
 #_resist = applied defense : if def > dmg then reduced damage
@@ -118,20 +139,31 @@ def fight():
             # User attack
 
             print("\n -----Round ", round, "-----")
+            time.sleep(2)
             print("\n You struck first!")
             a_dmg = multiply(a.attack,random.choice(range(0,6)))
             b_avoid = (b.defense + b.speed)*random.choice(avoid_values)
-            time.sleep(1)
-            print("\nYour damage points: "+str(a_dmg)+". Your speed points: "+str(a.speed))
-            time.sleep(1)
-            print("Opponent's hit points: "+str(b.hit_points)+". Opponent's avoid points: "+str(b_avoid))
+            # time.sleep(2)
+            # print("\nYour damage points: "+str(a_dmg))
+            # time.sleep(2)
+            # print("Opponent's hit points: "+str(b.hit_points))
+            time.sleep(3)
+
+
             if b_avoid > a.speed:
                 a_dmg = 0
                 print("\nThe attack missed.")
+            elif(a_dmg == 0):
+                print("\nThey successfully blocked your attack.")
             else:
-                b.hit_points = b.hit_points - a_dmg
+                block_amount = b.defense*random.choice(block_values)
+                b.hit_points = b.hit_points + (block_amount) - a_dmg
+                print("\nYou hit "+b.name+" for "+str(a_dmg)+", but they blocked "+str(block_amount))
 
-            time.sleep(1)
+
+            time.sleep(2)
+
+
             if b.hit_points <=0:
                 print("\n ~~~~~You win!~~~~~")
                 break
@@ -139,19 +171,30 @@ def fight():
                 print("\n ~~~~~You died:(~~~~~")
                 break
             time.sleep(2)
+
+
             print("\n Your opponent managed a hit.")
             b_dmg = multiply(b.attack,random.choice(range(0,6)))
             a_avoid = (a.defense + a.speed)*random.choice(avoid_values)
-            time.sleep(1)
-            print("\nOpponent damage points: "+str(b_dmg)+". Opponent speed points: "+str(b.speed))
-            time.sleep(1)
-            print("Your hit points: "+str(a.hit_points)+". Your resist points: "+str(a_avoid))
+            # time.sleep(2)
+            # print("\nOpponent damage points: "+str(b_dmg))
+            # time.sleep(2)
+            # print("Your hit points: "+str(a.hit_points))
+            time.sleep(3)
+
+
             if a_avoid > b.speed:
                 b_dmg = 0
                 print("\nThe attack missed.")
+            elif(b_dmg == 0):
+                print("\nYou successfully blocked their attack.")
             else:
-                a.hit_points = a.hit_points - b_dmg
-            time.sleep(1)
+                block_amount = a.defense*random.choice(block_values)
+                a.hit_points = a.hit_points + (block_amount) - b_dmg
+                print("\nYou  were hit  for "+str(b_dmg)+", but blocked "+str(block_amount))
+
+
+            time.sleep(2)
             time.sleep(2)
             round = round+1
             print("\nEnd of round!")
@@ -159,32 +202,48 @@ def fight():
             b.speed = random.choice(ads_values)
             time.sleep(1.5)
             printSelf()
+            print("\n Loading . . .")
             time.sleep(4)
             printOpponent()
+            print("\n Loading . . .")
             time.sleep(4)
+
+
             if b.hit_points <=0:
                 print("\n ~~~~~You win!~~~~~")
                 break
             elif a.hit_points <=0:
                 print("\n ~~~~~You died:(~~~~~")
                 break
+            input("\nPress enter to advance to the next round")
+
+
         else:
 
             print("\n -----Round ", round,"-----")
+            time.sleep(2)
             print("\n Your opponent is faster..")
             b_dmg = multiply(b.attack,random.choice(range(0,6)))
             a_avoid = (a.defense + a.speed)*random.choice(avoid_values)
-            time.sleep(1)
-            print("\nOpponent damage points: "+str(b_dmg)+". Opponent speed points: "+str(b.speed))
-            time.sleep(1)
-            print("Your hit points: "+str(a.hit_points)+". Your resist points: "+str(a_avoid))
-            if a_avoid > b.speed:
-                a_dmg = 0
-                print("\nThe attack missed.")
-            else:
-                a.hit_points = a.hit_points - b_dmg
+            # time.sleep(2)
+            # print("\nOpponent damage points: "+str(b_dmg))
+            # time.sleep(2)
+            # print("Your hit points: "+str(a.hit_points))
+            time.sleep(3)
 
-            time.sleep(1)
+
+            if a_avoid > b.speed:
+                b_dmg = 0
+                print("\nThe attack missed.")
+            elif(b_dmg == 0):
+                print("\nYou successfully blocked their attack.")
+            else:
+                block_amount = a.defense*random.choice(block_values)
+                a.hit_points = a.hit_points + (a.defense*random.choice(avoid_values)) - b_dmg
+                print("\nYou were hit for "+str(b_dmg)+", but blocked "+str(block_amount))
+            time.sleep(2)
+
+
             if b.hit_points <=0:
                 print("\n~~~~~You win!~~~~~")
                 break
@@ -192,18 +251,28 @@ def fight():
                 print("\n~~~~~You lose:(~~~~~")
                 break
             time.sleep(2)
+
+
             print("\n ..but you won't go down without a fight!")
             a_dmg = multiply(a.attack,random.choice(range(0,6)))
             b_avoid = (b.defense + b.speed)*random.choice(avoid_values)
-            time.sleep(1)
-            print("\nYour damage points: "+str(a_dmg)+". Your speed points: "+str(a.speed))
-            time.sleep(1)
-            print("Opponent's hit points: "+str(b.hit_points)+". Opponent's resist points: "+str(b_avoid))
+            # time.sleep(2)
+            # print("\nYour damage points: "+str(a_dmg))
+            # time.sleep(2)
+            # print("Opponent's hit points: "+str(b.hit_points))
+            time.sleep(3)
+
+
             if b_avoid > a.speed:
                 a_dmg = 0
                 print("\nThe attack missed.")
+            elif(a_dmg == 0):
+                print("\nThey successfully blocked your attack.")
             else:
-                b.hit_points = b.hit_points - a_dmg
+                block_amount = b.defense*random.choice(block_values)
+                b.hit_points = b.hit_points + (block_amount) - a_dmg
+                print("\nYou hit "+b.name+" for "+str(a_dmg)+", but they blocked "+str(block_amount))
+
 
             time.sleep(2)
             round = round+1
@@ -212,15 +281,20 @@ def fight():
             b.speed = random.choice(ads_values)
             time.sleep(1.5)
             printSelf()
+            print("\n Loading . . .")
             time.sleep(4)
             printOpponent()
-            time.sleep(4)
+            print("\n Loading . . .")
+            time.sleep(2.5)
+
+
             if b.hit_points <=0:
                 print("\n ~~~~~You win!~~~~~")
                 break
             elif a.hit_points <=0:
                 print("\n ~~~~~You died:(~~~~~")
                 break
+            input("\nPress enter to advance to the next round")
 
 create()
 create_opponents()
